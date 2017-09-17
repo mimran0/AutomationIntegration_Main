fff94e08dd68eed50ed96ebbeec9715a4434ac61
mimran0 <jmrthan@gmail.com>
2017-09-17 17:20:57 -0400

    Initial Commit

---
+++ Youtube/src/test/java/HiddenLove.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+
+public class HiddenLove {
+    public static void main(String[] args) {
+        //below are some sample code
+        String isExist;
+        System.out.println("I love you more when you look at my that");
+        String st="I love you more when you look at my that";
+        boolean ifExist=st.contains("love");
+        //below is a "if condition" which is written for fun
+        if(ifExist==true){
+            System.out.println("Yes, love is there");
+             isExist="Yes";
+        }else {
+            System.out.println("There is no love");
+            isExist="No";
+        }
+        //below line indicates that Junit jar file is configured as dependency
+        Assert.assertEquals("Yes",isExist);
+
+    }
+}
---
+++ Youtube/src/test/java/OpenLove.java
@@ -0,0 +1,5 @@
+public class OpenLove {
+    public static void main(String[] args) {
+        System.out.println("I love you Openly.");
+    }
+}
